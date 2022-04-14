b6b8bee61a734b098196dcc08ac610afcbcaced6
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-04-14 23:33:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2147483647;
+        float b = a;
+        System.out.println("Значение int: " + a + "\n" +
+                "Значение автоматического преобразования в float: " + b);
+    }
+}
